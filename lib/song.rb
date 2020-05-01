4453f563ed31927e3624aaafd2bc2e18e6736f90
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-04-30 21:27:15 -0700

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,56 @@
+class Song
+    attr_accessor :name, :artist, :genre
+    attr_reader :new_hash
+    
+    @@count = 0
+    @@genres = []
+    @@artists = []
+
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+        
+        @@genres << genre
+        @@artists << artist
+
+        @@count += 1
+    end
+
+    def self.count
+        @@count
+    end
+
+    def self.genres
+        @@genres.uniq!
+    end
+
+    def self.artists
+        @@artists.uniq!
+    end
+
+    def self.genre_count
+        new_hash = {}
+        @@genres.each_with_index do |key|
+            if new_hash[key]
+                new_hash[key] += 1
+            else
+                new_hash[key] = 1
+            end
+        end
+        return new_hash
+    end
+
+    def self.artist_count
+        new_hash = {}
+        @@artists.each_with_index do |key|
+            if new_hash[key]
+                new_hash[key] += 1
+            else
+                new_hash[key] = 1
+            end
+        end
+        return new_hash
+    end
+
+end
