f9b7976638573da2b90fe366204d38794b568452
IfeAkiWad <ifeoluwa.wade@gmail.com>
2020-05-04 22:32:10 -0500

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,29 @@
+require 'Pry'
+
+class Song
+    attr_accessor :name, :artist, :genre
+    @@count = 0
+    @@artists = []
+    @@genres = []
+
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+    end
+    # def initialize
+    #     @@count += 1
+    # end
+    
+    def self.count
+    @@count
+    end
+
+    def self.artists
+        @@artists
+    end
+
+    def self.genres
+        @@genres
+    end
+end
