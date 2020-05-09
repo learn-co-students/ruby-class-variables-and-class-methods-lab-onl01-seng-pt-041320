0fa4e453eec541e607f92e65f57620c489388315
Jonah Mallard <benjaminjmallard@gmail.com>
2020-05-09 23:37:18 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,31 @@
+class Song 
+  attr_accessor :name, :artist_name
+  @@all = []
+  
+  def self.create
+    song = self.new 
+    @@all << song 
+    song 
+  end 
+  
+  def self.all 
+    @@all 
+  end
+  
+  def save 
+    self.class.all << self 
+  end 
+  
+  def self.new_by_name(song_name)
+    song = self.new
+    song.name = song_name 
+    song 
+  end 
+  
+  def self.create_by_name(song_name)
+    song_name = self.create
+    @@all << song_name 
+    
+  end 
+  
+end 
